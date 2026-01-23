8da6bd7dd322aef6fe4150e8ae76433a81b85f2a
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-01-24 00:49:26 +0100

    v1.1.20

--- core.lua
+++ core.lua
@@ -6,7 +6,7 @@ ArmoryUtils:OnEvent(
 	function()
 		ArmoryUtils:UnregisterEvent(auf, "PLAYER_LOGIN")
 		ArmoryUtils:SetAddonOutput("ArmoryUtils", 134952)
-		ArmoryUtils:SetVersion(134952, "1.1.19")
+		ArmoryUtils:SetVersion(134952, "1.1.20")
 		if AUTAB == nil then
 			AUTAB = AUTAB or {}
 		end
