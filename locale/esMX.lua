f5ddee59316438d4980eba14b2b499cf29cfaada
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-08-11 09:18:31 +0200

    v1.1.71

---
+++ locale/esMX.lua
@@ -0,0 +1,9 @@
+-- esMX Mexican Spanish
+local _, ArmoryUtils = ...
+ArmoryUtils:AddTrans("esMX", "LID_GENERAL", "General")
+ArmoryUtils:AddTrans("esMX", "LID_SHOWMINIMAPBUTTON", "Mostrar botón del minimapa")
+ArmoryUtils:AddTrans("esMX", "LID_TEXTSIZES", "Tamaños de texto")
+ArmoryUtils:AddTrans("esMX", "LID_ILVLFONTSIZE", "Tamaño del texto de nivel de objeto: %s")
+ArmoryUtils:AddTrans("esMX", "LID_SIDEFONTSIZE", "Tamaño de los textos laterales: %s")
+ArmoryUtils:AddTrans("esMX", "LID_TOOLTIP", "Información")
+ArmoryUtils:AddTrans("esMX", "LID_SHOWITEMLEVEL", "Mostrar nivel de objeto")
---
+++ locale/zhTW.lua
@@ -0,0 +1,9 @@
+-- zhTW Traditional Chinese
+local _, ArmoryUtils = ...
+ArmoryUtils:AddTrans("zhTW", "LID_GENERAL", "常規")
+ArmoryUtils:AddTrans("zhTW", "LID_SHOWMINIMAPBUTTON", "顯示小地圖按鈕")
+ArmoryUtils:AddTrans("zhTW", "LID_TEXTSIZES", "文字大小")
+ArmoryUtils:AddTrans("zhTW", "LID_ILVLFONTSIZE", "裝備等級文字大小: %s")
+ArmoryUtils:AddTrans("zhTW", "LID_SIDEFONTSIZE", "側邊文字大小: %s")
+ArmoryUtils:AddTrans("zhTW", "LID_TOOLTIP", "提示框")
+ArmoryUtils:AddTrans("zhTW", "LID_SHOWITEMLEVEL", "顯示裝備等級")
