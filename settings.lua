d82a55844a291e49dd7027480de72b29c0ce805a
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-09-17 10:37:29 +0200

    v1.3.2

--- settings.lua
+++ settings.lua
@@ -1,6 +1,6 @@
 local _, ArmoryUtils = ...
 local ICON = 134952
-local VERSION = "1.3.1"
+local VERSION = "1.3.2"
 local DEFAULT_WIDTH = 460
 local DEFAULT_HEIGHT = 520
 local DEFAULT_ILVLFONTSIZE = 11
